c7b78d3bc04c849d17c399528bca24f3db6a9f49
hibtmimi <hibatamimi24@gmail.com>
2020-11-24 00:32:15 +0200

    in process

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 //import User from '../components/User';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import reportWebVitals from './reportWebVitals';
 
